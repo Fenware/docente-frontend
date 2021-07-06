9f72c61d8a3fe693843f816e667b69748df62e0c
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-06 15:52:07 -0300

    feat: Añadiendo registro al router y ordenando codigo

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 import store from "../store";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 import Subjects from "../views/Subjects.vue";
-import Orientations from "../views/Orientations.vue";
 
 const routes = [
-  { path: "/", redirect: "/inicio" },
+  {
+    path: "/registro",
+    name: "Register",
+    component: Register
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
   {
     path: "/inicio",
     name: "Home",
@@ -14,23 +23,12 @@ const routes = [
     meta: { requireAuth: true },
   },
   {
-    path: "/login",
-    name: "Login",
-    component: Login,
-    meta: { requireAuth: false },
-  },
-  {
     path: "/materias",
     name: "Subjects",
     component: Subjects,
     meta: { requireAuth: true },
   },
-  {
-    path: "/orientaciones",
-    name: "Orientations",
-    component: Orientations,
-    meta: { requireAuth: true },
-  },
+  { path: "/", redirect: "/inicio" },
 ];
 
 const router = createRouter({
@@ -47,7 +45,7 @@ router.beforeEach((to, from, next) => {
     // ruta protegida es true
     // token es nulo true, por ende redirigimos al inicio
     next({ name: "Login" });
-  } else if (to.fullPath === "/login" && store.state.token !== null) {
+  } else if ((to.fullPath === "/login" || to.fullPath === "/registro") && store.state.token !== null) {
     // En caso contrario sigue...
     next({ name: redirectedFrom != undefined ? redirectedFrom : "Home" });
   } else {
