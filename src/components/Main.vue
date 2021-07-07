d9e04c4b85efc12c5eac316b75d44923b787ded6
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-07 20:46:11 -0300

    fix: Sincronizando el token en main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,23 @@
 <template>
-  <main class="mx-auto px-3" v-if="isLoginView()">
+  <main class="w-full mx-auto px-3" v-if="isLoginView()">
       <router-view />
   </main>
 </template>
 
 <script>
+
+import { mapActions } from "vuex";
+
 export default {
   name: "MainComponent",
   methods: {
+    ...mapActions(["syncToken", "checkSession"]),
     isLoginView() {
       return this.$route.name != "Login" || this.$route.name != "Register";
     },
+  },
+  created() {
+    this.syncToken();
   },
 };
 </script>
