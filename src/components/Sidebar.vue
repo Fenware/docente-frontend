c610111cf72fbc93b5245daf9c725312e054f061
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-26 16:34:47 -0300

    feat: Boton para actualizar consultas

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,23 +2,33 @@
   <div
     class="w-72 text-white h-full rounded-3xl | bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl"
   >
-    <h3 class=" px-1 text-xl text-center pt-2">Consultas pendientes</h3>
-    <div class="mt-3 p-2 h-5/6 overflow-y-auto">
-      <ConsultationsContainer/>
+    <div class="truncate">
+      <h3 class="font-bold px-1 text-xl text-center pt-2">Consultas pendientes</h3>
+    </div>
+    <div class="flex justify-start items-center mt-2">
+      <button 
+      @click="syncConsultations()"
+      class="bg-gray-700 text-xs mx-2 px-2  rounded-full border-2 border-gray-400">
+        Actualizar
+        <i class="fas fa-sync-alt "></i>
+      </button>
+    </div>
+    <div class=" p-2 h-5/6 overflow-y-auto">
+      <ConsultationsContainer />
     </div>
   </div>
 </template>
 
 <script>
-import ConsultationsContainer from '@/components/ConsultationsContainer.vue';
-import { mapActions } from 'vuex';
+import ConsultationsContainer from "@/components/ConsultationsContainer.vue";
+import { mapActions } from "vuex";
 export default {
   name: "SidebarComponent",
-  components:{
+  components: {
     ConsultationsContainer,
   },
-  methods:{
-    ...mapActions(["syncConsultations"])
+  methods: {
+    ...mapActions(["syncConsultations"]),
   },
   created() {
     this.syncConsultations();
