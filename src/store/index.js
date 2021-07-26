299f2e065470b4d03dfad415d2411a524486927c
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-26 00:29:32 -0300

    feat: Nuevas variables y funciones para obtener consultas y mensajes

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,11 @@ export default createStore({
       subject_name: "Esperando datos",
       teacher_name: "Esperando datos",
       theme: "Esperando datos",
+      body: "Esperando datos",
+      messages: [],
     },
+    new_message: "",
+    new_message_mode: false,
     consultations: [],
     subjects: [],
     subjects_selected: [],
@@ -118,8 +122,30 @@ export default createStore({
     setConsultations(state, consultations) {
       state.consultations = consultations;
     },
+    addConsultation(state, consultation) {
+      state.consultations.push(consultation);
+    },
     setConsultation(state, consultation) {
       state.consultation = consultation;
+    },
+    removeConsultation(state, id_consultation) {
+      state.consultations.forEach((consultation, index) => {
+        if (parseInt(consultation.id) == id_consultation) {
+          state.consultations.splice(index, 1);
+        }
+      });
+    },
+    setNewMessage(state, message) {
+      state.new_message = message;
+    },
+    toogleNewMessageMode(state) {
+      state.new_message_mode = !state.new_message_mode;
+    },
+    setConsultationBody(state, body) {
+      state.consultation.body = body;
+    },
+    setConsultationMessages(state, messages) {
+      state.consultation.messages = messages;
     },
   },
   actions: {
@@ -351,7 +377,56 @@ export default createStore({
         .catch((error) => {
           console.log(error);
         });
-    }
+    },
+    async sendConsultationMessage(
+      { state, dispatch, commit },
+      id_consultation
+    ) {
+      let data = {
+        consulta: id_consultation,
+        msg: state.new_message,
+      };
+      await axios({
+        method: "post",
+        url: state.API_URL + "/consulta-mensaje",
+        data: data,
+        headers: state.headers,
+      })
+        .then((res) => {
+          commit("toogleNewMessageMode");
+          if (res.data == 1) {
+            dispatch(
+              "getConsultationMessages",
+              parseInt(state.consultation.id)
+            );
+          } else {
+            alert(res.data.result.error_msg);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async getConsultationMessages({ state, commit }, id) {
+      let data = `consulta=${id}`;
+      await axios({
+        method: "get",
+        url: state.API_URL + `/consulta-mensaje?${data}`,
+        headers: state.headers,
+      })
+        .then((res) => {
+          if (Array.isArray(res.data) && res.data.length > 0) {
+            commit("setConsultationBody", res.data[0].content);
+            res.data.splice(0, 1);
+            commit("setConsultationMessages", res.data);
+          } else {
+            console.log("Error: getConsultationMessages ->" + res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   getters: {
     subjectsFiltered(state) {
