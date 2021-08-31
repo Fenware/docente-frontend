11eec8bce1cd2acc23c39e75b39ddd13c9ef54f4
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-31 11:33:17 -0300

    refactor: Moviendo state, actions y mutations a modulo de consultas

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,19 @@
 import { createStore } from "vuex";
-import axios from "axios";
 import ab from "@/utils/websockets";
 
 // Modulos
 import auth from "./modules/auth";
 import groups from "./modules/groups";
+import consultations from "./modules/consultations";
 
 export default createStore({
   modules: {
     auth,
     groups,
+    consultations
   },
   state: {
     API_URL: process.env.VUE_APP_ROOT_API,
-    consultation: {
-      creation_date: "Esperando datos",
-      group_name: "Esperando datos",
-      id: "Esperando datos",
-      state: "1",
-      subject_name: "Esperando datos",
-      teacher_name: "Esperando datos",
-      theme: "Esperando datos",
-      body: "Esperando datos",
-      messages: [],
-    },
-    new_message: "",
-    new_message_mode: false,
-    consultations: [],
     user: {
       id: null,
       ci: null,
@@ -79,107 +66,10 @@ export default createStore({
       state.user["second_surname"] = "";
       state.user["email"] = "";
     },
-    setConsultations(state, consultations) {
-      state.consultations = consultations;
-    },
-    addConsultation(state, consultation) {
-      state.consultations.push(consultation);
-    },
-    setConsultation(state, consultation) {
-      state.consultation = consultation;
-    },
-    removeConsultation(state, id_consultation) {
-      state.consultations.forEach((consultation, index) => {
-        if (parseInt(consultation.id) == id_consultation) {
-          state.consultations.splice(index, 1);
-        }
-      });
-    },
-    setNewMessage(state, message) {
-      state.new_message = message;
-    },
-    toogleNewMessageMode(state) {
-      state.new_message_mode = !state.new_message_mode;
-    },
-    setConsultationBody(state, body) {
-      state.consultation.body = body;
-    },
-    setConsultationMessages(state, messages) {
-      state.consultation.messages = messages;
-    },
-    setConsultationStateAnswered(state) {
-      state.consultation.state = 2;
-    },
   },
   actions: {
     searcher({ commit }, payload) {
       commit("setText", payload.toLowerCase());
-    },
-    async syncConsultations({ state, commit }) {
-      await axios({
-        method: "get",
-        url: state.API_URL + "/consulta",
-        headers: state.headers,
-      })
-        .then((res) => {
-          if (Array.isArray(res.data)) {
-            commit("setConsultations", res.data);
-          } else {
-            console.log("Error: syncConsultations ->" + res.data);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    async sendConsultationMessage(
-      { state, dispatch, commit },
-      id_consultation
-    ) {
-      let data = {
-        consulta: id_consultation,
-        msg: state.new_message,
-      };
-      await axios({
-        method: "post",
-        url: state.API_URL + "/consulta-mensaje",
-        data: data,
-        headers: state.headers,
-      })
-        .then((res) => {
-          commit("toogleNewMessageMode");
-          if (res.data == 1) {
-            dispatch(
-              "getConsultationMessages",
-              parseInt(state.consultation.id)
-            );
-          } else {
-            alert(res.data.result.error_msg);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    async getConsultationMessages({ state, commit }, id) {
-      let data = `consulta=${id}`;
-      await axios({
-        method: "get",
-        url: state.API_URL + `/consulta-mensaje?${data}`,
-        headers: state.headers,
-      })
-        .then((res) => {
-          if (Array.isArray(res.data) && res.data.length > 0) {
-            commit("setConsultationBody", res.data[0].content);
-            res.data.splice(0, 1);
-            commit("setConsultationMessages", res.data);
-          } else {
-            console.log("Error: getConsultationMessages ->" + res.data);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
     },
     wsConnection() {
       var conn = new ab.Session(
