58efad3bdd1ebf1a41eaf1c6cca536a637222ff7
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-05 22:14:40 -0300

    feat: Clase para mostrar los iconos de google

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,34 @@ body {
   height: 100vh;
   width: 100vw;
 }
+
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
+
+
 /* ----------------- Buttons ----------------- */
 
 .btn-success {
