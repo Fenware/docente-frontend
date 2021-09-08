8924871423a61aebc4fc6df509b4166ee2c04464
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-08 01:23:40 -0300

    feat: Funcion para eliminar horario

--- src/store/modules/schedule.js
+++ src/store/modules/schedule.js
@@ -12,9 +12,16 @@ export default {
     setSchedule(state, schedule) {
       state.schedule = schedule;
     },
-    pushSchedule(state, schedule){
+    pushSchedule(state, schedule) {
       state.schedule.push(schedule);
-    }
+    },
+    removeSchedule(state, day) {
+      state.schedule.foreach((schedule, index) => {
+        if (parseInt(schedule.day) == day) {
+          state.schedule.splice(1, index);
+        }
+      });
+    },
   },
   actions: {
     async getSchedule({ rootState, commit }) {
@@ -34,7 +41,7 @@ export default {
       let data = {
         day: schedule.day,
         start_hour: schedule.start_hour,
-        end_hour: schedule.end_hour
+        end_hour: schedule.end_hour,
       };
       await axios({
         method: "post",
@@ -43,8 +50,24 @@ export default {
         headers: rootState.headers,
       })
         .then((res) => {
-          if(res.data == 1){
+          if (res.data == 1) {
             commit("pushSchedule", schedule);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async removeSchedule({ rootState, commit }, day) {
+      await axios({
+        method: "delete",
+        url: rootState.API_URL + "/schedule",
+        day,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (res.data == 1) {
+            commit("removeSchedule", day);
           }
         })
         .catch((error) => {
