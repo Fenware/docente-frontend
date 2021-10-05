bcb98df007f4c11e11b4ccb3f73f9057a1fba013
lucaspintos909 <lucaspintos909@gmail.com>
2021-10-05 09:41:11 -0300

    refactor: Cambiando a los endpoints nuevos

--- src/store/modules/groups.js
+++ src/store/modules/groups.js
@@ -3,7 +3,7 @@ import axios from "axios";
 /* import router from "@/router/index"; */
 
 // Modulo donde manejo las alertas
-import showAlert from "@/utils/alerts";
+import { showAlert } from "@/utils/alerts";
 
 export default {
   state: {
@@ -31,8 +31,8 @@ export default {
   actions: {
     async getTeacherGroups({ rootState, commit }) {
       await axios({
-        method: "get",
-        url: rootState.API_URL + "/user-grupo",
+        method: "post",
+        url: rootState.API_URL + "/user-group/getGroups",
         headers: rootState.headers,
       })
         .then((res) => {
@@ -59,23 +59,21 @@ export default {
       };
       await axios({
         method: "post",
-        url: rootState.API_URL + "/user-grupo",
-        data: data,
+        url: rootState.API_URL + "/user-group/takeGroup",
+        data,
         headers: rootState.headers,
       })
         .then((res) => {
           if (res.data == 1) {
-            showAlert({ type: "success", message: "Has tomado el grupo correctamente!" });
+            showAlert({
+              type: "success",
+              message: "Has tomado el grupo correctamente",
+            });
             dispatch("getTeacherGroups");
-          } else if (res.data == 0) {
-            showAlert({ type: "error", message: "Ya has tomado este grupo!" });
-            console.log("Error: takeGroup -> " + res.data);
-          } else {
-            showAlert({ type: "error", message: res.data });
-            // Para cuando felipe lo arregle
-            /* showAlert({ type: "error", message: res.data.result.error_msg }); */
-            console.log("Error: takeGroup -> " + res.data);
-          }
+          } else if (res.data == 0)
+            showAlert({ type: "error", message: "Ya has tomado este grupo" });
+          else showAlert({ type: "error", message: res.data.result.error_msg });
+          /* showAlert({ type: "error", message: res.data }); */
         })
         .catch((error) => {
           console.log(error);
@@ -85,53 +83,53 @@ export default {
       let data = {
         grupo: parseInt(group.id),
       };
-      console.log(data);
       await axios({
-        method: "delete",
-        url: rootState.API_URL + "/user-grupo",
-        data: data,
+        method: "post",
+        url: rootState.API_URL + "/user-group/leaveGroup",
+        data,
         headers: rootState.headers,
       })
         .then((res) => {
-          console.log(res);
           if (res.data == 1) {
             commit("removeGroup", group.id);
-            showAlert({ type: "info", message: `Te has dado de baja del grupo ${group.full_name} correctamente`});
-          } else {
-            console.log("Error: deleteGroup -> " + res.data);
-          }
+            showAlert({
+              type: "info",
+              message: `Te has dado de baja del grupo ${group.full_name}`,
+            });
+          } else console.log("Error: deleteGroup -> " + res.data);
         })
         .catch((error) => {
           console.log(error);
         });
     },
     async getTeacherSubjectsTakenByGroup({ rootState, commit }, group_id) {
-      let data = `group=${parseInt(group_id)}`
+      let data = { group: parseInt(group_id) };
       await axios({
-        method: "get",
-        url: rootState.API_URL + `/user-materia?${data}`,
+        method: "post",
+        url: rootState.API_URL + `/user-group/getUserSubjects`,
+        data,
         headers: rootState.headers,
       })
         .then((res) => {
           if (Array.isArray(res.data)) {
             commit("setSubjectsTaken", res.data);
           } else {
-            console.log("Error: setTeacherSubjectsTaken -> " + res.data);
+            console.log("Error: getTeacherSubjectsTakenByGroup -> " + res.data);
           }
         })
         .catch((error) => {
           console.log(error);
         });
     },
-    async takeSubject({rootState, commit}, payload) {
+    async takeSubject({ rootState }, payload) {
       let data = {
         grupo: parseInt(payload.group_id),
         materia: parseInt(payload.subject_id),
       };
       await axios({
         method: "post",
-        url: rootState.API_URL + "/user-materia",
-        data: data,
+        url: rootState.API_URL + "/user-group/takeSubject",
+        data,
         headers: rootState.headers,
       })
         .then((res) => {
@@ -146,15 +144,15 @@ export default {
           console.log(error);
         });
     },
-    async unsuscribeGroupSubject({rootState, commit}, payload) {
+    async unsuscribeGroupSubject({ rootState, commit }, payload) {
       let data = {
         grupo: parseInt(payload.group_id),
         materia: parseInt(payload.subject_id),
       };
       await axios({
-        method: "delete",
-        url: rootState.API_URL + "/user-materia",
-        data: data,
+        method: "post",
+        url: rootState.API_URL + "/user-group/leaveSubject",
+        data,
         headers: rootState.headers,
       })
         .then((res) => {
